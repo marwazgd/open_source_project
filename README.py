de5283a6d61614376d11f08ee15f5c889f0b0ef4
marwa zeghoudi <marwazegoudi04@gmail.com>
2025-12-12 22:53:31 +0100

    Update README.py

--- README.py
+++ README.py
@@ -23,6 +23,7 @@ print(df,"\n")
 
 #2.a)opération sue les tableaux :
 print("-------------Opération-------------","\n")
+
 #2.b)Sélectionner la colonne "Longueur":
 lng = df["Longueur"]
 print(lng,"\n")
@@ -44,6 +45,7 @@ print("-------------Ajouter d'une colonne-------------","\n")
 df["Catégorie GC"] = df["Pourcentage GC"].apply(lambda x: "Riche" if x > 55 else "Moyen" if 45 <= x <= 55 else "Faible")
 print(df)
 print("\n")
+
 #6)Ajouter une colonne donnant le nombre de G dans chaque séquence: 
 df["Nombre de G"] = df["Séquence"].str.count("G") 
 print("-------------Nombre de G-------------","\n") 
@@ -55,6 +57,7 @@ ecarttype_GC = df["Pourcentage GC"].std()
 ecarttype_longeure = df["Longueur"].std()
 print(f"ecart type de pourcentage de GC : {ecarttype_GC: .2f}")
 print(f"ecart type de Longueur : {ecarttype_longeure: .2f}""\n")
+
 #8)Sauvegarde le tableau final dans un fichier CSV:
 df.to_csv("Tableau_séquences.csv",index=False)
 
